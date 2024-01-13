8ed82cab4c4c1e17172263f1bfdc5165ff2c35d1
Chandrasekar <chandrasekar51200@gmail.com>
2024-01-13 13:41:49 +0530

    added project section and made some modifications

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const githubCode = document.querySelector(".project--githubLink1");
+const websiteLink = document.querySelector(".project--link1");
+
+githubCode.addEventListener("mouseover", function() {
+    githubCode.style.color = "#147EFB";
+    document.querySelector(".gitCodeImage").style.stroke = "#147EFB";
+})
+
+githubCode.addEventListener("mouseout", function() {
+    githubCode.style.color = "#000";
+    document.querySelector(".gitCodeImage").style.stroke = "#000";
+})
+
+websiteLink.addEventListener("mouseover", function() {
+    websiteLink.style.color = "#147EFB";
+    document.querySelector(".websiteLink").style.stroke = "#147EFB";
+})
+
+websiteLink.addEventListener("mouseout", function() {
+    websiteLink.style.color = "#000";
+    document.querySelector(".websiteLink").style.stroke = "#000";
+})
