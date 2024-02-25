1fde85ef41123b5a4ffdf7fb44ad2d5ce5b376b2
Chandrasekar <chandrasekar51200@gmail.com>
2024-02-25 13:16:36 +0530

    navbar features enables

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 const githubCode = document.querySelector(".project--githubLink1");
 const websiteLink = document.querySelector(".project--link1");
 
-githubCode.addEventListener("mouseover", function() {
-    githubCode.style.color = "#147EFB";
-    document.querySelector(".gitCodeImage").style.stroke = "#147EFB";
-})
+githubCode.addEventListener("mouseover", function () {
+  githubCode.style.color = "#147EFB";
+  document.querySelector(".gitCodeImage").style.stroke = "#147EFB";
+});
 
-githubCode.addEventListener("mouseout", function() {
-    githubCode.style.color = "#000";
-    document.querySelector(".gitCodeImage").style.stroke = "#000";
-})
+githubCode.addEventListener("mouseout", function () {
+  githubCode.style.color = "#000";
+  document.querySelector(".gitCodeImage").style.stroke = "#000";
+});
 
-websiteLink.addEventListener("mouseover", function() {
-    websiteLink.style.color = "#147EFB";
-    document.querySelector(".websiteLink").style.stroke = "#147EFB";
-})
+websiteLink.addEventListener("mouseover", function () {
+  websiteLink.style.color = "#147EFB";
+  document.querySelector(".websiteLink").style.stroke = "#147EFB";
+});
 
-websiteLink.addEventListener("mouseout", function() {
-    websiteLink.style.color = "#000";
-    document.querySelector(".websiteLink").style.stroke = "#000";
-})
+websiteLink.addEventListener("mouseout", function () {
+  websiteLink.style.color = "#000";
+  document.querySelector(".websiteLink").style.stroke = "#000";
+});
